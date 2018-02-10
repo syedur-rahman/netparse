c825d49c58621d13b59fb941a874d68f6c635dc5
Syedur Rahman <syedur.work@gmail.com>
2018-02-10 15:46:51 -0500

    Changed DL URL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Syedur Rahman',
   author_email = 'syedur.work@gmail.com',
   url = 'https://github.com/syedur-rahman/netparse', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/syedur-rahman/netparse/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['automation', 'network-engineering', 'parse'], # arbitrary keywords
   classifiers = [],
 )
