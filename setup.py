a805641527a88e0120cf8e029f46fae4e10d7570
Syedur Rahman <syedur.work@gmail.com>
2018-02-10 15:15:50 -0500

    Updating setup parameters

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
-
 setup(
-    name='NetParse',
-    version='0.1dev',
-    packages=['netparse',],
-    license='Apache License 2.0',
-    long_description='Lowers Frustration with NetEng Automation Efforts',
+  name = 'netparse',
+  packages = ['netparse'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Automatic NetEng CLI Parser.',
+  author = 'Syedur Rahman',
+  author_email = 'syedur.work@gmail.com',
+  url = 'https://github.com/syedur-rahman/netparse', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['automation', 'network-engineering', 'parse'], # arbitrary keywords
+  classifiers = [],
 )
